c568c0d43a72c52dee9abfed043d551aa0c764c2
aimz <aimz@DESKTOP-0OFMO86>
2023-11-21 22:03:09 +0330

    Add project files.

---
+++ DataBaseConnection/AppDb/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DataBaseConnection.Entities;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBaseConnection.AppDb
+{
+    public class AppDbContext : DbContext
+    {
+        
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=DataBaseConnection;Integrated Security=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+        
+        public DbSet<product> products { get; set; }
+    }
+}
---
+++ DataBaseConnection/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using DataBaseConnection.AppDb;
+using DataBaseConnection.Entities;
+using DataBaseConnection.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DataBaseConnection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _appDbContext;
+
+        public HomeController(ILogger<HomeController> logger, AppDbContext appDbContext)
+        {
+            _logger = logger;
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductList()
+        {
+            return View(_appDbContext.products.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(product product)
+        {
+            _appDbContext.products.Add(product);
+            _appDbContext.SaveChanges();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
